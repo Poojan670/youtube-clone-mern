40e8b97f0f8d8cfd7d4f58640f8343b13bd180ba
Poojan670 <po0janhunt@gmail.com>
2022-09-11 17:11:16 +0545

    made node js as the sole backend

--- backend/django/src/core/current_user.py
+++
@@ -1,6 +0,0 @@
-def get_created_by(context_with_request):
-    request = context_with_request('request', None)
-    if not request.user.is_anonymous:
-        return request.user
-    else:
-        return ""
